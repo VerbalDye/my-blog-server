24ea3e007c305dfde16650f7d616e5aee3527016
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-23 16:37:48 -0400

    added basic homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require('../models');
+const sequelize = require('../config/connection');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
+        ],
+        order: [['created_at', 'DESC']],
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    })
+        .then(dbPostData => {
+             
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts, loggedIn:req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+module.exports = router;
