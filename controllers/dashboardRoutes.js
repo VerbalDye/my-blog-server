cd4a664078883d1365e435a27d69af9f2aebe8ae
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-24 16:30:36 -0400

    added basic dashboard routes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'title', 'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
+        ],
+        order: [['created_at', 'DESC']],
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findAll()
+})
+
+module.exports = router;
